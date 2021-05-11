a080fc322f1dbd8f574c11b907fd2fd07743fc97
Edson Matsimbe <m.edsonjorge@gmail.com>
2021-05-11 08:18:46 +0200

    setup dev dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports={
     
         entry:'./src/app.js',
         output:{
-            path:path.join(__dirname ,'\public'),
+            path:path.join(__dirname ,'\public','dist'),
             filename:'bundle.js'
         },
         module:{
